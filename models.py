2b28f0a888b66c8afe63cbf391b490e7cbb0c491
mirbek1 <109955612+mirbek1@users.noreply.github.com>
2022-08-10 19:12:28 +0600

    add models

---
+++ models.py
@@ -0,0 +1,29 @@
+from peewee import *
+from datetime import datetime
+
+db = PostgresqlDatabase(
+    'flask_db',
+    host = 'localhost',
+    port = '5432',
+    user = 'flask_user',
+    password = '123'
+)
+db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Post(BaseModel):
+    title = CharField(max_length = 255, null = False)
+    description = CharField(max_length = 255, null = False)
+    date = DateField(default = datetime.now)
+
+    def __repr__(self):
+        return self.title
+
+# db.create_tables([Post])
+
+
+
+db.close()  
